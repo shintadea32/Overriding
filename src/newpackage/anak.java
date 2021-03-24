1532c7dbfae825998322917490a15e2866e7c90e
shintadewi29rpl <shinta_dewi_29rpl@student.smktelkom-mlg.sch.id>
2021-03-24 21:29:04 +0700

    overriding

---
+++ src/newpackage/anak.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author I d e a P a d
+ */
+public class anak extends induk {
+    
+    public void nama(){
+        System.out.println("|                        KTP                        |");
+    }
+    
+}
---
+++ src/newpackage/induk.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+/**
+ *
+ * @author I d e a P a d
+ */
+public class induk {
+    public void nama(){
+        System.out.println("|Nama                 : Shinta Dewi Agustina        |");  
+    }
+    public void nik(){
+        System.out.println("|NIK                  : 3452167                     |");
+    }
+    public void ttl(){
+        System.out.println("|Tempat Tanggal Lahir : Tulungagung, 14 Agustus 2004|");
+    }
+    public void jeniskel(){
+        System.out.println("|Jenis Kelamin        : Perempuan                   |");
+    }
+    public void agama(){
+        System.out.println("|Agama                : Islam                       |");
+    }
+    
+}
