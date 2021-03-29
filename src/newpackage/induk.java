6b17cf9e1f25f82446c97eba33e871e711d3a2c8
shintadewi29rpl <72966161+shintadewi29rpl@users.noreply.github.com>
2021-03-29 07:38:36 +0700

    Update induk.java

--- src/newpackage/induk.java
+++ src/newpackage/induk.java
@@ -10,18 +10,25 @@ package newpackage;
  * @author I d e a P a d
  */
 public class induk {
+//induk merupakan class utama
+    
+    //overridin method dengan judul nama
     public void nama(){
         System.out.println("|Nama                 : Shinta Dewi Agustina        |");  
     }
+    //overridin method dengan judul nik
     public void nik(){
         System.out.println("|NIK                  : 3452167                     |");
     }
+    //overridin method dengan judul ttl
     public void ttl(){
         System.out.println("|Tempat Tanggal Lahir : Tulungagung, 14 Agustus 2004|");
     }
+    //overridin method dengan judul jeniskel
     public void jeniskel(){
         System.out.println("|Jenis Kelamin        : Perempuan                   |");
     }
+    //overridin method dengan judul agama
     public void agama(){
         System.out.println("|Agama                : Islam                       |");
     }
